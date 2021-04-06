b55d1ff719934c9023f04b5fc625c55d95a996d1
Razin Idzuddin <razin99@gmail.com>
2021-04-06 21:54:10 +1000

    made it prettier, now it has a loader animation (from loading.io)

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 document.querySelector("button").addEventListener("click", () => {
   const div = document.getElementById("content");
+  div.textContent = "";
+  const loader = document.createElement("div");
+  loader.setAttribute("class", "lds-ripple");
+  loader.appendChild(document.createElement("div"));
+  loader.appendChild(document.createElement("div"));
+  div.appendChild(loader);
   fetch("http://whatthecommit.com/index.txt")
     .then((r) => {
       if (!r.ok) throw r;
